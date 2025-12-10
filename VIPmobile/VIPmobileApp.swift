a62bdf1e87a593076197b4e23ace5baffe8dd048
Joshua Khooba <joshuak419@gmail.com>
2025-12-10 17:19:33 -0500

    Initial Commit

---
+++ VIPmobile/VIPmobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPmobileApp.swift
+//  VIPmobile
+//
+//  Created by Joshua Khooba on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VIPmobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
